883f137620759c8519860d7c40d4d7e24228451f
FlavioFCarvalho <flaviocfr@gmail.com>
2021-06-03 14:08:23 -0300

    Novos tipos de retorno.

--- src/main/java/com/algaworks/curso/jpa2/consultas/ConsultasAgregadasEmCarro.java
+++ src/main/java/com/algaworks/curso/jpa2/consultas/ConsultasAgregadasEmCarro.java
@@ -6,7 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-import com.algaworks.curso.jpa2.modelo.Carro;
+import com.algaworks.curso.jpa2.consultas.info.AluguelCarroInfo;
 import com.algaworks.curso.jpa2.util.JPAUtil;
 
 public class ConsultasAgregadasEmCarro {
@@ -15,20 +15,20 @@ public class ConsultasAgregadasEmCarro {
 		EntityManagerFactory emf = JPAUtil.createEntityManager().getEntityManagerFactory();
 		EntityManager em = emf.createEntityManager();
 		
-		String jpql = "select c, count(a), max(a.valorTotal), avg(a.valorTotal) "
-			  	          + "from Carro c JOIN c.alugueis a "
-			  	          + "group by c "
-			  	          + "having count(a) >= 1";
+		String jpql = "select NEW com.algaworks.curso.jpa2.consultas.info.AluguelCarroInfo(c, count(a), "
+				+ " max(a.valorTotal), avg(a.valorTotal))"
+				+ " from Carro c JOIN c.alugueis a"
+				+ "  group by c   having count(a) > 1";
 		
-		List<Object[]> resultados = em.createQuery(jpql).getResultList();
-		for (Object[] obj : resultados) {
-			System.out.println("Modelo: " + ((Carro) obj[0]).getModelo().getDescricao());
-			System.out.println("Quantidade de alugueis: " + obj[1]);
-			System.out.println("Valor máximo: " + obj[2]);
-			System.out.println("Valor médio: " + obj[3]);
-			System.out.println();
+		
+		List<AluguelCarroInfo> resultados = em.createQuery(jpql, AluguelCarroInfo.class).getResultList();
+		for (AluguelCarroInfo aci : resultados) {
+		    System.out.println("Modelo: " + aci.getCarro().getModelo().getDescricao());
+		    System.out.println("Quantidade de alugueis: " + aci.getTotalAlugueis());
+		    System.out.println("Valor máximo: " + aci.getValorMaximo());
+		    System.out.println("Valor médio: " + aci.getValorMedio());
+		    System.out.println();
 		}
-		
 		em.close();
 	}
 	
